c329a47a750c9e98a098f3a9e6a281a4bb40221d
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-05-15 14:50:19 +0300

    efef

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const startBtn = document.querySelector('[data-start]');
+//console.log(startBtn);
+const endBtn = document.querySelector('[data-stop]');
+//console.log(endBtn);
+
+let timerId = null;
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+startBtn.addEventListener('click', () => {
+     timerId = setInterval(() => {
+        const body = document.getElementsByTagName('body')[0];
+        body.style.backgroundColor = "" + getRandomHexColor();
+        //console.log(body);
+    }, 1000);
+});
+
+
+endBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    console.log('Stop ! ');
+})
