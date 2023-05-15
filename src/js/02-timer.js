bca5abf51e7c0c9b29e4cce07bf5d67a9724788a
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-05-15 16:54:38 +0300

    cam gata
    cu un mic bug la notificari

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,72 @@
 import flatpickr from "flatpickr";
+let currentDate = new Date();
+let selectedDate = "";
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      selectedDate = selectedDates[0];
+      if (selectedDate <= currentDate)
+        window.alert('Select a valid future date');
+      else {
+        document.querySelector('[data-start]').disabled = false;
+      }
+    },
+  };
 
-console.log(flatpickr);
+const inputStart = document.getElementById('datetime-picker');
+//console.log(inputStart);
+
+flatpickr(inputStart, options);
+
+
+const startBtn = document.querySelector('[data-start]');
+
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+
+const addZero = (string) => {
+    if (string.length == 1)
+        return "0" + string;
+    else return string;
+};
+
+const cronometru = () => {
+    const timerId = setInterval(()=>{
+        currentDate = new Date();
+        let time = selectedDate - currentDate;
+        let {days, hours, minutes, seconds } = convertMs(time);
+        const dataDays = document.querySelector('[data-days]');
+        const dataHours = document.querySelector('[data-hours]');
+        const dataMinutes = document.querySelector('[data-minutes]');
+        const dataSeconds = document.querySelector('[data-seconds]');
+
+
+        dataDays.innerHTML = addZero("" + days);
+        dataHours.innerHTML = addZero("" + hours);
+        dataMinutes.innerHTML = addZero("" + minutes);
+        dataSeconds.innerHTML = addZero("" + seconds);
+    }, 1000);
+}
+
+startBtn.addEventListener('click', cronometru);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,69 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from "notiflix";
+
+
+
+
+const delayInput = document.querySelector('[data-delay]');
+const stepInput = document.querySelector('[data-step]');
+const amountInput = document.querySelector('[data-amount]');
+
+const submitBtn = document.querySelector('[data-submit]');
+
+let delay, amount, step;
+
+delayInput.addEventListener('input', () => {
+  delay = delayInput.value;
+  //console.log(delay);
+  delay = parseInt(delay);
+});
+
+amountInput.addEventListener('input', () => {
+  amount = amountInput.value;
+ // console.log(amount);
+ amount = parseInt(amount);
+});
+
+
+stepInput.addEventListener('input', () => {
+  step = stepInput.value;
+  //console.log(step);
+  step = parseInt(step);
+});
+
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  setTimeout(() => {
+    let totalDelay = delay;
+    for (let i = 1; i <= amount; i ++) {
+      //creeam o promisiune
+      
+      const shouldResolve = Math.random() > 0.5;
+
+      let p = new Promise((resolve, reject) => {
+        if (shouldResolve)
+          setTimeout(resolve, totalDelay);
+        else 
+          setTimeout(reject, totalDelay);
+      });
+
+      p.then((message)=>{
+        Notiflix.Notify.success(
+          `Woa, everything good in ${message}`,
+          {
+            timeout:totalDelay,
+          }
+        )
+      }).catch((message)=>{
+        Notiflix.Notify.failure(
+          `Eh, everything wrond in ${message}`,
+          {
+            timeout:totalDelay,
+          }
+        )
+      });
+      totalDelay += step;
+    }
+}, delay);
+});
+
